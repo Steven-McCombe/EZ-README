04e4875a8452e3e414be347ffd752b6b740f3a8d
Steven-McCombe <smccombe93@gmail.com>
2022-11-26 12:45:16 -0500

    added working links to contact section, removed some comments

---
+++ utils/README.md
@@ -0,0 +1,37 @@
+# README
+
+## Description
+N/A
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contribute](#contribute)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+N/A
+
+## Usage
+N/A
+
+## License 
+  ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg) 
+
+  [Read more about MIT License here.](https://opensource.org/licenses/MIT)
+  
+  
+
+## Contribute
+N/A
+
+## Tests
+N/A
+
+## Questions
+Contact Links:
+- [Github Profile](https://github.com/N/A)
+- [E-mail Me](mailto:noemail@nomail.com)
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,47 +1,33 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 
-// "Apache License 2.0"
-// "GNU General Public License v3.0"
-// case "MIT License":
-// `"BSD 2-Clause "Simplified" License"`
-// `"BSD 3-Clause "New" or "Revised" License"`
-// "Boost Software License 1.0"
-// "Creative Commons Zero v1.0 Universal"
-// "Eclipse Public License 2.0"
-// "GNU Affero General Public License v2.0"
-// "GNU General Public License v2.0"
-// "GNU Lesser General Public License v2.1"
-// "Mozilla Public License 2.0"
-// "The Unlicense"
-
 // If there is no license, return an empty string
 function renderLicenseBadge(projLicense) {
   
   switch (projLicense) {
     case "Apache License 2.0":
-      return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]"
+      return "![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)"
     case "GNU General Public License v3.0":
-      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]"
+      return "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)"
     case "MIT License":
-      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]"
+      return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)"
     case `"BSD 2-Clause "Simplified" License"`:
-      return "[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)]"
+      return "![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)"
     case `"BSD 3-Clause "New" or "Revised" License"`:
-      return "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]"
+      return "![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)"
     case "Boost Software License 1.0":
-      return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)]"
+      return "![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)"
     case "Creative Commons Zero v1.0 Universal":
-      return "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)]"
+      return "![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)"
     case "Eclipse Public License 2.0":
-      return "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)]"
+      return "![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)"
     case "GNU Affero General Public License v3.0":
-      return "[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)]"
+      return "![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)"
     case "GNU General Public License v2.0":
-      return "[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)]"
+      return "![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)"
     case "GNU Lesser General Public License v3.0":
-      return "[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)]"
+      return "![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)"
     case "Mozilla Public License 2.0":
-      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)]"
+      return "![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)"
     case "The Unlicense":
       return "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)]"
     default: ""
@@ -84,7 +70,18 @@ function renderLicenseLink(projLicense) {
 };
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(projLicense) {
+  if (projLicense.length === 0) {
+    return ""
+  } else {
+    return `## License 
+  ${renderLicenseBadge(projLicense)} 
+
+  [Read more about ${projLicense} here.]${renderLicenseLink(projLicense)}
+  
+  `
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -107,8 +104,7 @@ ${data.projInstall}
 ## Usage
 ${data.projUsage}
 
-## License
-${data.license} 
+${renderLicenseSection(data.projLicense)}
 
 ## Contribute
 ${data.projContribute}
@@ -117,8 +113,9 @@ ${data.projContribute}
 ${data.projTest}
 
 ## Questions
-${data.projGit}
-${data.projEmail}
+Contact Links:
+- [Github Profile](https://github.com/${data.projGit})
+- [E-mail Me](mailto:${data.projEmail})
 
 `;
 }
