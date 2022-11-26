4f86b687c42af3ea98dcfced44b2c4399fc0f427
Steven-McCombe <smccombe93@gmail.com>
2022-11-26 12:04:37 -0500

    added switch case for licenses. populates link and badge.

--- utils/index.js
+++ utils/index.js
@@ -62,8 +62,8 @@ const questions = [
         type: "list",
         name: "projLicense",
         message: "Please Choose a License",
-        choices: ["Apache License 2.0", "GNU General Public License v3.0", "MIT License", `"BSD 2-Clause "Simplified" License"`, `"BSD 3-Clause "New" or "Revised" License"`, "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Eclipse Public License 2.0", "GNU Affero General Public License v2.0", "GNU General Public License v2.0", "GNU Lesser General Public License v2.1", "Mozilla Public License 2.0", "The Unlicense"],
-        default: "N/A"
+        choices: ["Apache License 2.0", "GNU General Public License v3.0", "MIT License", `"BSD 2-Clause "Simplified" License"`, `"BSD 3-Clause "New" or "Revised" License"`, "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Eclipse Public License 2.0", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU Lesser General Public License v3.0", "Mozilla Public License 2.0", "The Unlicense"],
+        default: ""
     },
     {
         type: "input",
